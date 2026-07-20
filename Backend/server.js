67b38e94c3c26eebd6755dd3e267e034c2905a21
Akshit-Singh-00 <khush227799@gmail.com>
2026-07-20 12:32:38 +0530

    Restructure project into Backend folder

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 
-const app = require("../src/app");
-const connectDB = require("../src/config/database");
+const app = require("./src/app");
+const connectDB = require("./src/config/database");
 
 connectDB();
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(` Server is running on port ${PORT}`);
+    console.log(`🚀 Server is running on port ${PORT}`);
 });
