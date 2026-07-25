be976136670caf106ac02ca6d0674eb623ab1a81
Akshit-Singh-00 <khush227799@gmail.com>
2026-07-25 13:22:49 +0530

    Fixed bug

--- Frontend/src/features/auth/hooks/useAuth.js
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -1,13 +1,13 @@
 import { useContext } from "react";
 import { AuthContext } from "../auth.context";
-import { login, register, logout } from "../services/auth.api";
+import { login, register, logout, getMe } from "../services/auth.api";
 
 export const useAuth = () => {
     const context = useContext(AuthContext);
 
     if (!context) {
         throw new Error("useAuth must be used within AuthProvider");
-    }
+    }               
 
     const { user, setUser, loading, setLoading } = context;
 
